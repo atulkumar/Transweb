59211cce83ead1d49988b054efcc2099a2f18d54
Aayush Atul Sirpal Aarav <Aayush & Aarav>
2016-09-11 13:17:03 +0530

    First draft

---
+++ TransWeb/TransWeb/Controllers/IndustriesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TransWeb.Controllers
+{
+    public class IndustriesController : Controller
+    {
+        // GET: Industries
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Insurence()
+        {
+            return View("Insurence");
+        }
+    }
+}
---
+++ TransWeb/TransWeb/Controllers/TechController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TransWeb.Controllers
+{
+    public class TechController : Controller
+    {
+        // GET: Tech
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Database()
+        {
+            return View("Database");
+        }
+    }
+}
