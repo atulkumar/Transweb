67d1c70e9f2421d2972e41a6031a573f23193c3f
atul <atul.kumar@spanservices.com>
2016-11-24 10:02:53 +0530

    Home page,Contact Us Changes after Review

--- TransWeb/TransWeb/Controllers/HomeController.cs
+++ TransWeb/TransWeb/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net.Mail;
 using System.Web.Mvc;
 
 namespace TransWeb.Controllers
@@ -16,13 +14,73 @@ namespace TransWeb.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Hello";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string name, string email, string message)
+        {
+            try
+            {
+                using (var mail = new MailMessage())
+                {
+                    mail.From = new MailAddress(email);
+                    mail.To.Add(new MailAddress("info@transcendentpro.com"));
+                    mail.Bcc.Add(new MailAddress("atulsirpal@gmail.com"));
+                    mail.Subject = "Enquiry";
+                    mail.Body = message;
+                    mail.IsBodyHtml = true;
+
+                    try
+                    {
+                        SmtpClient client = new SmtpClient();
+                        client.Send(mail);
+                    }
+                    finally
+                    {
+                        //dispose the client
+                        mail.Dispose();
+                    }
+                }
+            }
+            catch (SmtpFailedRecipientsException ex)
+            {
+                foreach (SmtpFailedRecipientException t in ex.InnerExceptions)
+                {
+                    var status = t.StatusCode;
+                    if (status == SmtpStatusCode.MailboxBusy ||
+                        status == SmtpStatusCode.MailboxUnavailable)
+                    {
+                        Response.Write("Delivery failed - retrying in 5 seconds.");
+                        System.Threading.Thread.Sleep(5000);
+                        //resend
+                        //SmtpClient client = new SmtpClient();
+                        //client.Send(mail);
+                    }
+                    else
+                    {
+                    }
+                }
+            }
+            catch (SmtpException Se)
+            {
+                // handle exception here
+                //  Response.Write(Se.ToString());
+                ViewBag.Message = Se.ToString();
+            }
+            catch (Exception ex)
+            {
+                // Response.Write(ex.ToString());
+            }
+
+            ViewBag.Message = "Thank you! We will contact you soon.";
 
             return View();
         }
